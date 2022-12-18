6bf31f95a75fd6f55f30122ec7d4ba576ddf1fd2
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2022-12-19 00:29:15 +0530

    #include<stdio.h> int main(){     int n,sum=0,product=1,last_digit;     scanf("%d", &n);     int temp = n;     while( n > 0 ) {         last_digit = n % 10;         sum = sum + last_digit;         product = product * last_digit;         n = n / 10;
        } 
        if(sum == product) { 
            printf("Spy Number");
            }
            else {
                printf("Not Spy Number");
                }
                return 0;
        
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 int main(){
-    int n,i,s=0,p=1;
-    scanf("%d",&n); 
-    while(n>0)
-    { i=n%10; 
-    s=s+i;
-    p=p*i;
-    n=n/10; 
+    int n,sum=0,product=1,last_digit;
+    scanf("%d", &n);
+    int temp = n; 
+    while( n > 0 ) {
+        last_digit = n % 10;
+        sum = sum + last_digit;
+        product = product * last_digit;
+        n = n / 10; 
         
-    }
-    if(s==p)
-    { printf("Spy Number");
-    }
-    else {
-        printf("Not Spy Number"); }
+    } 
+    if(sum == product) { 
+        printf("Spy Number");
+        }
+        else {
+            printf("Not Spy Number");
+            }
+            return 0;
     
 }
