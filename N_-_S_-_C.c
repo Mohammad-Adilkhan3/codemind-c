df86c6599ad58432c42d2999f3b89c673c990c99
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2022-12-20 23:25:09 +0530

    #include<stdio.h> int main(){     int n,a,b;     scanf("%d%d",&a,&b);     for(n=a+1;n<b;n++) {         printf("%d %d %d ",n,n*n,n*n*n);         }
    }

--- N_-_S_-_C.c
+++ N_-_S_-_C.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main(){ 
-    int n,a,b; 
+    int n,a,b;
     scanf("%d%d",&a,&b);
-    for(n=a+1;n<b;n++) {
+    for(n=a+1;n<b;n++) { 
         printf("%d %d %d
 ",n,n*n,n*n*n);
-        }}
+        }
+    
+}
