513b9f8e7bff35439be53e352047287277da844c
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-01-01 23:00:52 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++) {         i=n*(n+1)/2;         printf("%d",i);         }}

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main(){ 
+int main(){
     int n,i;
     scanf("%d",&n);
     for(i=1;i<=n;i++) {
-        i=n*(n+1)/2; 
+        i=n*(n+1)/2;
         printf("%d",i);
         }}
