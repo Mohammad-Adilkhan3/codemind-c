1353ae24e2ac1eaa89d0aa59ab98cc308b097563
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-01-11 23:00:55 +0530

    #include<stdio.h> int main(){     int n,i,j,sq,c1=0,c2=0;     scanf("%d",&n);     sq=n*n;     while(n>0) {         i=n%10;         c1++;         n=n/10;
        } 
        while(sq>0) { 
            j=sq%10;
            c2++;
            sq=sq/10; 
            if(c1==c2) {
                break;
                }
                }
                if(i==j) { 
                    printf("Automorphic Number");
                    }
                    else {
                        printf("Not an Automorphic Number");
                        }}

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -3,25 +3,23 @@ int main(){
     int n,i,j,sq,c1=0,c2=0;
     scanf("%d",&n);
     sq=n*n;
-    while(n>0) { 
+    while(n>0) {
         i=n%10; 
         c1++;
         n=n/10; 
         
-    }
-    while(sq>0) {
-        j=sq%10; 
-        c2++; 
+    } 
+    while(sq>0) { 
+        j=sq%10;
+        c2++;
         sq=sq/10; 
         if(c1==c2) {
-            break; 
-            
-        }
-        }
-        if(i==j) { 
-            printf("Automorphic Number");
+            break;
             }
-            else {
-                printf("Not an Automorphic Number"); 
-                
-            }}
+            }
+            if(i==j) { 
+                printf("Automorphic Number");
+                }
+                else {
+                    printf("Not an Automorphic Number");
+                    }}
