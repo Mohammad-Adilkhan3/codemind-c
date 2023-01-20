2bd0f4555578b2efe2a3f8f719a55d299f325321
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-01-20 23:21:42 +0530

    #include<stdio.h> int main(){     int a,n,i,m=0;     scanf("%d",&n);while(n>0) { a=n%10; if(m<a) { m=a; } n=n/10; } printf("%d",m); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,13 +1,4 @@
 #include<stdio.h>
 int main(){
-    int a,n,m=0,i;
-    scanf("%d",&n);
-    while(n>0){
-        a=n%10;
-        if(m<a){
-        m=a;
-    }
-    n=n/10;
-    }
-    printf("%d",m);
-}
+    int a,n,i,m=0;
+    scanf("%d",&n);while(n>0) { a=n%10; if(m<a) { m=a; } n=n/10; } printf("%d",m); }
