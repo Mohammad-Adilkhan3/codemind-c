3eb6f0608e9527e8a32eaae8ceb9b9b2781b4766
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-01-25 23:18:54 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     if(n%3==1){         printf("HUGE");     }     else if (n%3==0){         printf("NORMAL");     }     else{         printf("SMALL");     } }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -1,2 +1,2 @@
-#include<stdio.h>
+#include<stdio.h> 
 int main(){     int n;     scanf("%d",&n);     if(n%3==1){         printf("HUGE");     }     else if (n%3==0){         printf("NORMAL");     }     else{         printf("SMALL");     } }
