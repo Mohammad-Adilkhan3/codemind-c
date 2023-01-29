b01f01a994411e3a0c296336561b087fc975e2fb
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-01-29 21:38:24 +0530

    #include<stdio.h> int main(){ int n,s=0,c,j=0,i; scanf("%d",&n); for(i=1;i<=n;i++) { s=s+i; j=j+i*i; } c=s*s-j; printf("%d",c); }

--- Difference_of_Sums.c
+++ Difference_of_Sums.c
@@ -1,11 +1,2 @@
 #include<stdio.h>
-int main(){ 
-    int n,s=0,c,j=0,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++) {
-        s=s+i;
-        j=j+i*i;
-        }
-        c=s*s-j;
-        printf("%d",c);
-        }
+int main(){ int n,s=0,c,j=0,i; scanf("%d",&n); for(i=1;i<=n;i++) { s=s+i; j=j+i*i; } c=s*s-j; printf("%d",c); }
