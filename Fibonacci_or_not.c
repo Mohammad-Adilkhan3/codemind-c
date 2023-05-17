bf322fc81d65167d5d304fc3a4c1d96d890d359c
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-05-17 21:53:08 +0530

    #include <stdio.h>   int main() {         int data, num1 = 0, num2 = 1, temp, flag = 0;         scanf("%d", &data);         if (data == num1 || data == num2) {                 printf("%d is a fibonacci number ", data);                 return 0;         }
            /* checking whether a given number is fobonacci no or not */
            while (num2 <= data) {
                    temp = num2;
                    num2 = num1 + num2;
                    num1 = temp;
                    if (num2 == data) {
                            flag = 1;
                            break;
                    }
            }
    
            /* print the results */
            if (flag) {
                    printf("True");
            } else {
                    printf("False");
            }
            return 0;
      }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,20 +1,30 @@
-#include<stdio.h>
-int main(){
-	int n,a=0,b=1,i,c,cnt=0;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++){
-		c=a+b;
-		if(c==n){
-			cnt++;
-			break;
-		}
-		a=b;
-		b=c;
-	}
-	if(cnt==0){
-		printf("False");
-	}
-	else{
-		printf("True");
-	}
-}
+#include <stdio.h>
+  int main() {
+        int data, num1 = 0, num2 = 1, temp, flag = 0;
+        scanf("%d", &data);
+        if (data == num1 || data == num2) {
+                printf("%d is a fibonacci number
+", data);
+                return 0;
+        }
+
+        /* checking whether a given number is fobonacci no or not */
+        while (num2 <= data) {
+                temp = num2;
+                num2 = num1 + num2;
+                num1 = temp;
+                if (num2 == data) {
+                        flag = 1;
+                        break;
+                }
+        }
+
+        /* print the results */
+        if (flag) {
+                printf("True");
+        } else {
+                printf("False");
+        }
+        return 0;
+  }
+
