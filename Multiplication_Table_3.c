1f1b798e91fccc2f524d37d7f66b43db3446c63a
Mohammad-Adilkhan3 <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-05-28 23:03:43 +0530

    #include <stdio.h> int main() {     int n1,i,n2,n3;     scanf("%d%d%d",&n1,&n2,&n3);     for (i=n2;i<=n3;i++) {         printf("%d x %d = %d ",n1,i,n1*i);         }         return 0;
    }

--- Multiplication_Table_3.c
+++ Multiplication_Table_3.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
-int main() { int n1,i,n2,n3; scanf("%d%d%d",&n1,&n2,&n3); for (i=n2;i<=n3;i++) { printf("%d x %d = %d
-",n1,i,n1*i); } return 0; }
+int main() { 
+    int n1,i,n2,n3;
+    scanf("%d%d%d",&n1,&n2,&n3);
+    for (i=n2;i<=n3;i++) {
+        printf("%d x %d = %d
+",n1,i,n1*i);
+        } 
+        return 0;
+    
+}
