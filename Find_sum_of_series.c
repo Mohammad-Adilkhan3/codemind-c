ced2f6441fc7ec99138245f7bd98732bdabc2fef
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-06-04 22:32:37 +0530

    #include<stdio.h> int main(){     float n,a=1,d=1;     float i,r=0;     scanf("%f",&n);     for (i=1;i<=n;i++) {         r=(1/i)+r;
        } 
        printf("%.2f",r);
        
    }

--- Find_sum_of_series.c
+++ Find_sum_of_series.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
-int main(){
+int main(){ 
     float n,a=1,d=1;
-    float i,r=0; 
+    float i,r=0;
     scanf("%f",&n);
     for (i=1;i<=n;i++) {
-        r=(1/i)+r;
-        }
-        printf("%.2f",r);
-        }
+        r=(1/i)+r; 
+        
+    } 
+    printf("%.2f",r);
+    
+}
