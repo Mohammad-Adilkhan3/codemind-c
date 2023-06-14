2b23a26cac9e6931d29046a1a9406ca64344d3c1
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-06-14 23:29:30 +0530

    #include<stdio.h> int main(){     int a,b,i;     scanf("%d%d",&a,&b);     for(i=1;i<=a*b;i++){         if(i%a==0 && i%b==0){             break;         }     }     printf("%d",i); }

--- LCM.c
+++ LCM.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main(){ 
-    int i,a,b;
+int main(){
+    int a,b,i;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=a*b;i++) {
-        if(i%a==0 && i%b==0)
-        break; 
-        
+    for(i=1;i<=a*b;i++){
+        if(i%a==0 && i%b==0){
+            break;
+        }
     }
     printf("%d",i);
-    }
+}
